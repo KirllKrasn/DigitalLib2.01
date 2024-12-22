1026aceab75d3a35001f7e7624b2b2351ecd1de8
gckil <gckil@PEKANYA>
2024-12-22 13:35:55 +0500

    Добавьте файлы проекта.

---
+++ DigitalLib2.0/MainWindow.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DigitalLib2._0
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bookAdder bookAdderEv;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            fillComboAuthor();
+            fillComboCategory();
+            fillComboPublisher();
+            fillComboGenre();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (bookAdderEv == null)
+            {
+                bookAdderEv = new bookAdder();
+                bookAdderEv.Show();
+            }
+            else
+            {
+                bookAdderEv.Activate();
+            }
+            if(!bookAdderEv.IsActive)
+            {
+                bookAdderEv = null;
+            }
+        }
+
+        private void fillComboAuthor()
+        {
+            // Заполение combobox автора
+            SqlConnection connection = new SqlConnection("Data Source=PEKANYA;Initial Catalog=DigitalLibrary;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;");
+            connection.Open();
+            SqlCommand command = new SqlCommand("SELECT DISTINCT Автор FROM Book", connection);
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                string author = reader.GetString(0);
+                comboAuthor.Items.Add(author);
+            }
+        }        
+        private void fillComboCategory()
+        {
+            // Заполение combobox категории
+            SqlConnection connection = new SqlConnection("Data Source=PEKANYA;Initial Catalog=DigitalLibrary;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;");
+            connection.Open();
+            SqlCommand command = new SqlCommand("SELECT DISTINCT Категории FROM Book", connection);
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                string category = reader.GetString(0);
+                comboCategory.Items.Add(category);
+            }
+        }        
+        private void fillComboPublisher()
+        {
+            // Заполение combobox категории
+            SqlConnection connection = new SqlConnection("Data Source=PEKANYA;Initial Catalog=DigitalLibrary;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;");
+            connection.Open();
+            SqlCommand command = new SqlCommand("SELECT DISTINCT Издатель FROM Book", connection);
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                string Publisher = reader.GetString(0);
+                comboPublisher.Items.Add(Publisher);
+            }
+        }        
+        private void fillComboGenre()
+        {
+            // Заполение combobox категории
+            SqlConnection connection = new SqlConnection("Data Source=PEKANYA;Initial Catalog=DigitalLibrary;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;");
+            connection.Open();
+            SqlCommand command = new SqlCommand("SELECT DISTINCT Жанр FROM Book", connection);
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                string Genre = reader.GetString(0);
+                comboGenre.Items.Add(Genre);
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged_2(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged_3(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+
+        private void DataGrid_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void BookList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DigitalLib2.0/bookAdder.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+//using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using static System.Net.Mime.MediaTypeNames;
+
+
+namespace DigitalLib2._0
+{
+    /// <summary>
+    /// Логика взаимодействия для bookAdder.xaml
+    /// </summary>
+    public partial class bookAdder : Window
+    {
+
+        public bookAdder()
+        {
+            InitializeComponent();
+        }
+
+        private string filePath;
+        private string fileName;
+
+        // SQL подключение
+
+        private void ChooseBook_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "PDF books | *.pdf";
+            dlg.Title = "Выберите книгу: ";
+            dlg.Multiselect = false;
+
+            bool? successOpen = dlg.ShowDialog();
+
+            if(successOpen == true)
+            {
+                filePath = dlg.FileName;
+                fileName = dlg.SafeFileName;
+            }
+            else
+            {
+                tbInfo.Content = "Файл не выбран: ";
+            }
+        }
+
+        private void AddBook_Click(object sender, RoutedEventArgs e)
+        {
+
+            string DirPath = "C:\\DigitalLib";
+            if (Directory.Exists(DirPath))
+            {
+                Directory.CreateDirectory(DirPath);
+            }
+
+            // Создание директории на диске при её отсутствии
+            string destinationPath = System.IO.Path.Combine(DirPath, fileName);
+
+            SqlConnection connection = new SqlConnection("Data Source=PEKANYA;Initial Catalog=DigitalLibrary;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;");
+            connection.Open();
+            SqlCommand changeID = new SqlCommand("");
+            SqlCommand command = new SqlCommand("Insert into Book(Автор, Год, Наименование, Издатель, Жанр, Категории,  Возрастное_ограничение, bookURL) values" +
+                "(@Автор, @Год, @Наименование, @Издатель, @Жанр, @Категории,  @Возрастное_ограничение, @bookURL)", connection);
+            var censure = Convert.ToInt32(TbCensure.Text);
+            command.Parameters.AddWithValue("@Автор", TbAuthor.Text);
+            command.Parameters.AddWithValue("@Год", TbYear.Text);
+            command.Parameters.AddWithValue("@Наименование", TbName.Text);
+            command.Parameters.AddWithValue("@Издатель", TbPublisher.Text);
+            command.Parameters.AddWithValue("@Жанр", TbGenre.Text);
+            command.Parameters.AddWithValue("@Категории", TbCategory.Text);
+            command.Parameters.AddWithValue("@Возрастное_ограничение", censure);
+            command.Parameters.AddWithValue("@bookURL", destinationPath);
+
+            command.ExecuteNonQuery();
+
+            MessageBox.Show(destinationPath);
+
+
+            //Проверяем, существует ли такой файл
+            if (File.Exists(destinationPath))
+            {
+                MessageBox.Show("Файл с таким именем уже существует в папке!", "Ошибка");
+                return;
+            }
+            // Перемещаем файл (можно также использовать File.Copy для копирования)
+            File.Move(filePath, destinationPath);
+
+            MessageBox.Show("Файл успешно перемещен!", "Успех");
+            filePath = null; // Сбрасываем путь после перемещения
+            tbInfo.Content = "Файл перемещён!"; // Обновляем UI
+        }
+
+    }
+}
